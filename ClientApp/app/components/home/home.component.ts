9fd21e0fbec89db50db8132d9faff9a24608f8e9
remy.villain <remy.villain@outlook.com>
2018-09-18 00:11:38 +0200

    impulse buttons

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -4,15 +4,18 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 declare var  AFRAME: any;
+declare var  CANNON: any;
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
     public ballContainer: any;
     public courseContainer: any;
+    public scene: any;
     public colors: any;
     public score: number = 0;
     public pars: number[] = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
@@ -26,8 +29,38 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
         this.ballContainer = document.querySelector("#container");
         this.courseContainer = document.querySelector("#course-container");
+        this.scene = document.querySelector("a-scene");
+        //this.scene.getPhysicsEngine().setTimeStep(1 / 120);
         this.colors = ["red", "orange", "yellow", "green", "blue", "purple", "hotpink"];
-        //this.loadCourse();
+        this.loadCourse();
+    }
+
+    public impulse(direction: number): void{
+        var el = this.scene.querySelector('#test') as any;
+
+        var x: number = 0; 
+        var y: number = 0; 
+        var z: number = 0; 
+
+        switch(direction){
+            case 0:
+                x = -0.5;
+            break;
+            case 1:
+                z = -0.5;
+            break;
+            case 2:
+                x = 0.5;
+            break;
+            case 3:
+            z = 0.5;
+            break;
+        }
+
+        el.body.applyImpulse(
+        /* impulse */        new CANNON.Vec3(x, y, z),
+        /* world position */ new CANNON.Vec3(0,0,0)
+        );
     }
 
     startGame(){
@@ -81,10 +114,9 @@ export class HomeComponent implements OnInit {
                                     let segZ = z + i*3;
                                     let segOrientation = 0;
                                     if(seg.isStart){
-                                        this.ballContainer.innerHTML += `<a-sphere color="yellow" radius="1"
-                                        draggable droppable grabbable
+                                        this.ballContainer.innerHTML += `<a-sphere color="white" radius="0.03"
                                         position="${segX+1.5} ${y+5} ${segZ-1.5}" 
-                                        dynamic-body="shape: box; mass: 0.5;"></a-sphere>`
+                                        dynamic-body="mass: 0.1; angularDamping: 0.7"></a-sphere>`
                                     }
                                     switch(seg.orientation){
                                         case 0:
